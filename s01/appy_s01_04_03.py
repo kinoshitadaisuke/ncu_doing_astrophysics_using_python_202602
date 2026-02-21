70bea3b987ba8f3cbaea77d4a22f772e558edb0d
Daisuke <mhdf23888@gmail.com>
2026-02-21 11:32:33 +0800

    adding "appy_s01_04_03.py" on 21/Feb/2026

--- s01/appy_s01_04_03.py
+++ s01/appy_s01_04_03.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/02 21:17:03 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/21 11:19:21 (UT+08:00) daisuke>
 #
 
 # making a dictionary
