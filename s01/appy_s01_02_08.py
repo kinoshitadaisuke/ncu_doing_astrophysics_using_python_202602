a263e530c68a7c10042ba25796ac86e5602c56e1
Daisuke <mhdf23888@gmail.com>
2026-02-21 12:40:17 +0800

    adding "appy_s01_02_08.py" on 21/Feb/2026

--- s01/appy_s01_02_08.py
+++ s01/appy_s01_02_08.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:18:21 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/21 11:18:13 (UT+08:00) daisuke>
 #
 
-# printing odd number between 0 and 30
-for i in range (30):
-    # if number is divisible by 2, then skipping to next number
-    if (i % 2 == 0):
-        continue
-    # if not, then it is an odd number
-    print (f'{i} is an odd number.')
+# importing math module
+import math
+
+# initialisation of a variable "a"
+a_deg = 0.0
+
+# calculating sin (0 deg), sin (15 deg), sin (30 deg), ..., sin (180 deg)
+while (True):
+    # stop "while" loop if "a_deg" is greater than 180.0 deg
+    if (a_deg > 180.0):
+        break
+    # converting from degree into radian
+    a_rad = math.radians (a_deg)
+    # calculation of sine
+    sin_a = math.sin (a_rad)
+    # printing result of calculation
+    print (f'sin ({a_deg:5.1f} deg) = {sin_a:8.6f}')
+    # incrementing variable "a"
+    a_deg += 15.0
