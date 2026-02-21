4a1678992e962cbaa3bcfab6c42148fcfcce08f0
Daisuke <mhdf23888@gmail.com>
2026-02-21 11:23:01 +0800

    adding "appy_s01_01_03.py" on 21/Feb/2026

---
+++ s01/appy_s01_01_03.py
@@ -0,0 +1,16 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/02 21:14:53 (UT+08:00) daisuke>
+#
+
+# two numbers
+a, b = 19, 2
+
+# calculation
+c = a / b
+
+# printing result of calculation
+print (f'a = {a}')
+print (f'b = {b}')
+print (f'c = a / b = {a} / {b} = {c}')
