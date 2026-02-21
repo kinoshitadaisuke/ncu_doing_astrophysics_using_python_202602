1e0ef2abb958e2bf241d81bfa094b0efe25a26c2
Daisuke <mhdf23888@gmail.com>
2026-02-21 11:29:23 +0800

    adding "appy_s01_01_01.py" on 21/Feb/2026

--- s01/appy_s01_01_01.py
+++ s01/appy_s01_01_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/02 21:14:43 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/21 11:14:21 (UT+08:00) daisuke>
 #
 
 # two numbers
