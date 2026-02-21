dcf78c327f7a3e67235a4a2c33e9fad96b93cac9
Daisuke <mhdf23888@gmail.com>
2026-02-21 11:28:55 +0800

    adding "appy_s01_00_09.py" on 21/Feb/2026

--- s01/appy_s01_00_09.py
+++ s01/appy_s01_00_09.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/02 21:14:03 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/21 11:13:02 (UT+08:00) daisuke>
 #
 
 # assignment of values to variables
