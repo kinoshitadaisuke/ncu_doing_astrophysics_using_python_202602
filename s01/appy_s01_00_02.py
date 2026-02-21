68bb2987293974a8cb2f5d4f337b05e1248587ce
Daisuke <mhdf23888@gmail.com>
2026-02-21 11:22:07 +0800

    adding "appy_s01_00_02.py" on 21/Feb/2026

---
+++ s01/appy_s01_00_02.py
@@ -0,0 +1,11 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/02 21:13:28 (UT+08:00) daisuke>
+#
+
+# assignment of a value to a variable
+location = 'Taiwan'
+
+# printing the value of a variable using built-in function "print ()"
+print ('I am in', location, 'now.')
