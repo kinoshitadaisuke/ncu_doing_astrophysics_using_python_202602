650b3e97cb6d30f3775520f3d9c19bdb1fc27e0a
Daisuke <mhdf23888@gmail.com>
2026-02-21 11:32:25 +0800

    adding "appy_s01_04_02.py" on 21/Feb/2026

--- s01/appy_s01_04_02.py
+++ s01/appy_s01_04_02.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/02 21:16:56 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/21 11:19:12 (UT+08:00) daisuke>
 #
 
 # making a set
