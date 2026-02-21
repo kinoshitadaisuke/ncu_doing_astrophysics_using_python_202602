9391ba773dc92a727ebf773cd73cb5548c6bf63e
Daisuke <mhdf23888@gmail.com>
2026-02-21 11:22:50 +0800

    adding "appy_s01_01_00.py" on 21/Feb/2026

---
+++ s01/appy_s01_01_00.py
@@ -0,0 +1,17 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/02 21:14:36 (UT+08:00) daisuke>
+#
+
+# two numbers
+a = 5
+b = 7
+
+# calculation
+c = a + b
+
+# printing result of calculation
+print (f'a = {a}')
+print (f'b = {b}')
+print (f'c = a + b = {a} + {b} = {c}')
