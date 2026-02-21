4cfe1f7ee28dfa01cd7f18c04d841372561b0066
Daisuke <mhdf23888@gmail.com>
2026-02-21 11:29:17 +0800

    adding "appy_s01_00_14.py" on 21/Feb/2026

--- s01/appy_s01_00_14.py
+++ s01/appy_s01_00_14.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/02 21:14:26 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/21 11:13:50 (UT+08:00) daisuke>
 #
 
 # assignment of variables
