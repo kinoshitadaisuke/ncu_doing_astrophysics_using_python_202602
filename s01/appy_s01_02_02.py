16ba5807146c78a36e1c65eb44717323e485f962
Daisuke <mhdf23888@gmail.com>
2026-02-21 11:23:42 +0800

    adding "appy_s01_02_02.py" on 21/Feb/2026

---
+++ s01/appy_s01_02_02.py
@@ -0,0 +1,16 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/02 21:15:58 (UT+08:00) daisuke>
+#
+
+# initialisation of a variable "total"
+total = 0
+
+# calculating 1 + 2 + 3 + ... + 10 using "for" statement
+for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    # adding "i" to "total"
+    total = total + i
+
+# printing result of calculation
+print (f'1 + 2 + 3 + ... + 8 + 9 + 10 = {total}')
