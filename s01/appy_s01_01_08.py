be8282be9c07ffb81e26203123d294d007b72229
Daisuke <mhdf23888@gmail.com>
2026-02-21 11:23:16 +0800

    adding "appy_s01_01_08.py" on 21/Feb/2026

---
+++ s01/appy_s01_01_08.py
@@ -0,0 +1,18 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/02 21:15:17 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+# a number
+a = 2.7183
+
+# calculation of natural logarithm
+b = math.log (a)
+
+# printing result of calculation
+print (f'a = {a}')
+print (f'b = log (a) = log ({a}) = {b}')
