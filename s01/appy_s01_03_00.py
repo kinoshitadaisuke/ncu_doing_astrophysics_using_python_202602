8cf876672ddd2b88edc206c601ea1a6b5dbaeef6
Daisuke <mhdf23888@gmail.com>
2026-02-21 11:31:57 +0800

    adding "appy_s01_03_00.py" on 21/Feb/2026

--- s01/appy_s01_03_00.py
+++ s01/appy_s01_03_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/02 21:16:33 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/21 11:18:38 (UT+08:00) daisuke>
 #
 
 # defining a function to calculate a square of given number
