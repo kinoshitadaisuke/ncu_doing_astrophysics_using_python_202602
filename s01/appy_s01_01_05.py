43a88328fc2b396483944b9d7017d3c65828e74c
Daisuke <mhdf23888@gmail.com>
2026-02-21 11:23:06 +0800

    adding "appy_s01_01_05.py" on 21/Feb/2026

---
+++ s01/appy_s01_01_05.py
@@ -0,0 +1,16 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/02 21:15:01 (UT+08:00) daisuke>
+#
+
+# two numbers
+a, b = 2, 8
+
+# calculation
+c = a**b
+
+# printing result of calculation
+print (f'a = {a}')
+print (f'b = {b}')
+print (f'c = a^b = {a}^{b} = {c}')
