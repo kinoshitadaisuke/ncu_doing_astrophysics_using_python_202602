56d57d54aaa434889d462f84a7826aac5ade1b11
Daisuke <mhdf23888@gmail.com>
2026-02-21 11:30:57 +0800

    adding "appy_s01_02_05.py" on 21/Feb/2026

--- s01/appy_s01_02_05.py
+++ s01/appy_s01_02_05.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/02 21:16:12 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/21 11:17:57 (UT+08:00) daisuke>
 #
 
 # initialisation of a variable "total"
