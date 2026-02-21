872664b23f946d595c75840165232df89312a1c8
Daisuke <mhdf23888@gmail.com>
2026-02-21 11:22:16 +0800

    adding "appy_s01_00_05.py" on 21/Feb/2026

---
+++ s01/appy_s01_00_05.py
@@ -0,0 +1,12 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/02 21:13:44 (UT+08:00) daisuke>
+#
+
+# assignment of a value to a variable
+a = 123.456
+
+# printing the value of a variable using built-in function "print ()"
+# and printf-style string formatting
+print ('a = %f' % a)
