86dcff8002983b2d38b533b89d6971b778ac297d
Daisuke <mhdf23888@gmail.com>
2026-02-28 15:41:39 +0800

    adding "s02/appy_s02_10_12.py" on 28/Feb/2026

---
+++ s02/appy_s02_10_12.py
@@ -0,0 +1,14 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/08 13:07:43 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+# sqrt () function
+b = math.sqrt (3)
+
+# printing result
+print (f'sqrt (3) = {b}')
