463e88f18385aee9cb80be6db51ad317d7ea65a9
Daisuke <mhdf23888@gmail.com>
2026-02-28 15:41:30 +0800

    adding "s02/appy_s02_10_06.py" on 28/Feb/2026

---
+++ s02/appy_s02_10_06.py
@@ -0,0 +1,22 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/08 13:06:11 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+# two integers "c" and "d"
+c = 30
+d = 45
+
+# gcd
+gcd_c_d = math.gcd (c, d)
+
+# printing results
+print (f'Use of gcd () function:')
+print (f'  c                                  = {c}')
+print (f'  d                                  = {d}')
+print (f'  greatest common divisor of c and d = {gcd_c_d}')
+print (f'')
