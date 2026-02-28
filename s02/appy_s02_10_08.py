a6b0c562805b44c31169dc854c2367b10653fb2a
Daisuke <mhdf23888@gmail.com>
2026-02-28 15:41:33 +0800

    adding "s02/appy_s02_10_08.py" on 28/Feb/2026

---
+++ s02/appy_s02_10_08.py
@@ -0,0 +1,18 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/08 13:06:44 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+# factorial
+factorial_5 = math.factorial (5)
+factorial_8 = math.factorial (8)
+
+# printing results
+print (f'Use of factorial () function:')
+print (f'  5! = 1 * 2 * 3 * 4 * 5\n     = {factorial_5}')
+print (f'  8! = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8\n     = {factorial_8}')
+print (f'')
