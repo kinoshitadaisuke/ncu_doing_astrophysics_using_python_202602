555d75bdc6364e5c955d0911f9cd8d83bfa6c319
Daisuke <mhdf23888@gmail.com>
2026-02-28 15:41:02 +0800

    adding "s02/appy_s02_06_00.py" on 28/Feb/2026

---
+++ s02/appy_s02_06_00.py
@@ -0,0 +1,11 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/08 12:54:29 (UT+08:00) daisuke>
+#
+
+# importing subprocess module
+import subprocess
+
+# executing a command "date"
+subprocess.run ('date')
