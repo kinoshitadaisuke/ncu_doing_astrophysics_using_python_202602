c5d790aac441c6fd1bde482df679fd3357cf5739
Daisuke <mhdf23888@gmail.com>
2026-03-01 09:50:25 +0800

    adding "appy_s02_12_02.py" on  1/Mar/2026

--- s02/appy_s02_12_02.py
+++ s02/appy_s02_12_02.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/08 13:11:10 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/28 16:10:53 (UT+08:00) daisuke>
 #
 
 # importing statistics module
