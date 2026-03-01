f453a82b30cb2e184cd93a089f788a96e28c472b
Daisuke <mhdf23888@gmail.com>
2026-03-01 09:49:12 +0800

    adding "appy_s02_01_00.py" on  1/Mar/2026

--- s02/appy_s02_01_00.py
+++ s02/appy_s02_01_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/08 12:47:08 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/28 16:00:34 (UT+08:00) daisuke>
 #
 
 # importing sys module
