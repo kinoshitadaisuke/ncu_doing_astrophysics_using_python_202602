543b346fed6c631c9f310cbba0408bbd6c090a29
Daisuke <mhdf23888@gmail.com>
2026-03-01 09:49:53 +0800

    adding "appy_s02_09_06.py" on  1/Mar/2026

--- s02/appy_s02_09_06.py
+++ s02/appy_s02_09_06.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/08 13:03:14 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/28 16:06:51 (UT+08:00) daisuke>
 #
 
 # importing re module
