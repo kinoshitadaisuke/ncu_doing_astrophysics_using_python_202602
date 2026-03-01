34540c6ae8a528ae6ec05963d59c556217621226
Daisuke <mhdf23888@gmail.com>
2026-03-01 09:50:12 +0800

    adding "appy_s02_10_13.py" on  1/Mar/2026

--- s02/appy_s02_10_13.py
+++ s02/appy_s02_10_13.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/08 13:07:58 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/28 16:09:01 (UT+08:00) daisuke>
 #
 
 # importing math module
