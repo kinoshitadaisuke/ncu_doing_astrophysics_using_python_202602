4b7a70f7d666eeb3a99ce7ef8c886d920a7dc100
Daisuke <mhdf23888@gmail.com>
2026-03-01 09:49:43 +0800

    adding "appy_s02_08_03.py" on  1/Mar/2026

--- s02/appy_s02_08_03.py
+++ s02/appy_s02_08_03.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/08 13:00:44 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/28 16:05:46 (UT+08:00) daisuke>
 #
 
 # importing datetime module
