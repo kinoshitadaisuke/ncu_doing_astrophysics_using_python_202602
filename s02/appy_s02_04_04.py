d6efd454d619aca30236fabcb4a5fc9f040093fa
Daisuke <mhdf23888@gmail.com>
2026-03-01 09:49:29 +0800

    adding "appy_s02_04_04.py" on  1/Mar/2026

--- s02/appy_s02_04_04.py
+++ s02/appy_s02_04_04.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/08 12:53:05 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/28 16:02:59 (UT+08:00) daisuke>
 #
 
 # importing pathlib module
