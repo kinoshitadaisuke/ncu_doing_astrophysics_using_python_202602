bc447a26b56c813c250835c17a18e7b88524f1fb
Daisuke <mhdf23888@gmail.com>
2026-03-01 09:50:38 +0800

    adding "appy_s02_15_01.py" on  1/Mar/2026

--- s02/appy_s02_15_01.py
+++ s02/appy_s02_15_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/08 13:19:00 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/28 16:12:35 (UT+08:00) daisuke>
 #
 
 # importing pint module
