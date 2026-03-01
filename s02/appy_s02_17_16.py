c59e9ed6b8a963c01bb69b93345a31a00d127304
Daisuke <mhdf23888@gmail.com>
2026-03-01 09:51:04 +0800

    adding "appy_s02_17_16.py" on  1/Mar/2026

--- s02/appy_s02_17_16.py
+++ s02/appy_s02_17_16.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/08 13:35:14 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/28 16:15:19 (UT+08:00) daisuke>
 #
 
 # importing sympy module
