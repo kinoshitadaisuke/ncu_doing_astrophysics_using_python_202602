32e157753f1ad0be0f60ddd873f42afa74475219
Daisuke <mhdf23888@gmail.com>
2026-03-01 09:50:35 +0800

    adding "appy_s02_14_01.py" on  1/Mar/2026

--- s02/appy_s02_14_01.py
+++ s02/appy_s02_14_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/08 13:17:54 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/28 16:12:03 (UT+08:00) daisuke>
 #
 
 # importing uncertainties module
