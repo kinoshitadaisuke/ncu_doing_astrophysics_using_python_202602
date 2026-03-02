f2c6d66c78ce87438776343618de3de1af9ada5b
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:20:06 +0800

    adding "appy_s02_15_03.py" on  2/Mar/2026

---
+++ s02/appy_s02_15_03.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:07:28 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+# two floats "a" and "b"
+a = 12.34
+b = -56.78
+
+# trunc
+a_trunc = math.trunc (a)
+b_trunc = math.trunc (b)
+
+# printing results
+print (f'Use of trunc () function:')
+print (f'  a         = {a}')
+print (f'  trunc (a) = {a_trunc}')
+print (f'  {a_trunc} is the integer part of {a}.')
+print (f'  b         = {b}')
+print (f'  trunc (b) = {b_trunc}')
+print (f'  {b_trunc} is the integer part of {b}.')
+print (f'')
