9281602126f85352688a37e8f1a48fa603eb072b
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:20:28 +0800

    adding "appy_s02_15_17.py" on  2/Mar/2026

---
+++ s02/appy_s02_15_17.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:09:45 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+# distance between two points
+coord_0  = (0.0, 1.0)
+coord_1  = (3.0, 2.0)
+dist_0_1 = math.dist (coord_0, coord_1)
+coord_2  = (0.0, 0.0, 0.0)
+coord_3  = (1.0, 1.0, 1.0)
+dist_2_3 = math.dist (coord_2, coord_3)
+
+# printing result
+print (f'coord_0 = {coord_0}')
+print (f'coord_1 = {coord_1}')
+print (f'distance between coord_0 and coord_1 = {dist_0_1}')
+print (f'coord_2 = {coord_2}')
+print (f'coord_3 = {coord_3}')
+print (f'distance between coord_2 and coord_3 = {dist_2_3}')
