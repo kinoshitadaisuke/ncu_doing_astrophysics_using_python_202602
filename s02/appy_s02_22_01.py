3f67b8d725e2ff694193e7501ebfacb3ae8ee14d
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:21:05 +0800

    adding "appy_s02_22_01.py" on  2/Mar/2026

---
+++ s02/appy_s02_22_01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:13:18 (UT+08:00) daisuke>
+#
+
+# importing sympy module
+import sympy
+
+# variable x
+x = sympy.symbols ('x')
+
+# function f
+f = x**2 + x - 2
+
+# factorisation of f
+f2 = sympy.factor (f)
+
+# printing result
+print (f'{f} = {f2}')
