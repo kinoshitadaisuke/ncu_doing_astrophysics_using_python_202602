f112cfa42bb79c5cbba428260d4d661747abdd56
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:19:14 +0800

    adding "appy_s02_09_02.py" on  2/Mar/2026

--- s02/appy_s02_09_02.py
+++ s02/appy_s02_09_02.py
@@ -1,27 +1,21 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:06:14 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/28 21:18:55 (UT+08:00) daisuke>
 #
 
-# importing gzip module
-import gzip
+# importing csv module
+import csv
 
-# file of Yale Bright Star Catalogue
-file_bsc = 'catalog.gz'
+# CSV file name
+file_csv = 'stars.csv'
 
-# output file
-file_output = 'bsc.data'
-
-# opening a compressed file
-with gzip.open (file_bsc, 'rb') as fh_read:
-    # reading file
-    data_byte = fh_read.read ()
-
-# converting byte data into string
-data_str = data_byte.decode ('utf-8')
-
-# opening new file
-with open (file_output, 'w') as fh_write:
-    # writing catalogue into a new file
-    fh_write.write (data_str)
+# opening file for reading
+with open (file_csv, 'r') as fh:
+    data_elements = csv.DictReader (fh)
+    # printing data
+    for line in data_elements:
+        print (f'{line["spectral type"]}')
+        print (f'  temperature [K]               : {line["temperature [K]"]}')
+        print (f'  absolute magnitude            : {line["absolute magnitude"]}')
+        print (f'  luminosity [solar luminosity] : {line["luminosity [solar luminosity]"]}')
