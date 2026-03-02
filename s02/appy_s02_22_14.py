545912fd17bbeef8ca5bc38baec155b61bfb2f1c
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:21:24 +0800

    adding "appy_s02_22_14.py" on  2/Mar/2026

---
+++ s02/appy_s02_22_14.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:15:00 (UT+08:00) daisuke>
+#
+
+# importing sympy module
+import sympy
+
+# variable x
+x = sympy.symbols ('x')
+
+# function f(x)
+f = sympy.exp (-x**2)
+
+# positive infinity
+pinf = sympy.oo
+
+# negative infinity
+ninf = -sympy.oo
+
+# integration of f(x)
+I = sympy.integrate (f, (x, ninf, pinf))
+
+# printing result
+print (f'I = {I}')
