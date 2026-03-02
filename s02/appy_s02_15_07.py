4452dd4e274795a8fa528130e3f3c0ef771a03af
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:20:11 +0800

    adding "appy_s02_15_07.py" on  2/Mar/2026

---
+++ s02/appy_s02_15_07.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:08:08 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+# two integers "c" and "d"
+c = 30
+d = 45
+
+# lcm
+lcm_c_d = math.lcm (c, d)
+
+# printing results
+print (f'Use of lcm () function:')
+print (f'  c                                = {c}')
+print (f'  d                                = {d}')
+print (f'  least common multiple of c and d = {lcm_c_d}')
+print (f'')
