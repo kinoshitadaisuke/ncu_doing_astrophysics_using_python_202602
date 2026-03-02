6af9c90035032078dc9858f2fa193b021f6debbf
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:20:09 +0800

    adding "appy_s02_15_05.py" on  2/Mar/2026

---
+++ s02/appy_s02_15_05.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:07:53 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+# two floats "a" and "b"
+a = 12.34
+b = -56.78
+
+# fabs
+a_abs = math.fabs (a)
+b_abs = math.fabs (b)
+
+# printing results
+print (f'Use of fabs () function:')
+print (f'  a                   = {a}')
+print (f'  absolute value of a = {a_abs}')
+print (f'  b                   = {b}')
+print (f'  absolute value of b = {b_abs}')
+print (f'')
