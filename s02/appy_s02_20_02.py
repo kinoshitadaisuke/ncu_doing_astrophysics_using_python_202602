fb324d86391de38681b3bb55c041aed7c4898b6a
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:21:01 +0800

    adding "appy_s02_20_02.py" on  2/Mar/2026

---
+++ s02/appy_s02_20_02.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:12:42 (UT+08:00) daisuke>
+#
+
+# importing pint module
+import pint
+
+# units
+ur           = pint.UnitRegistry ()
+u_km         = ur.km
+u_sec        = ur.sec
+u_km_per_sec = u_km / u_sec
+
+# velocity
+v = 300.0 * u_km_per_sec
+
+# time
+t = 10.0 * u_sec
+
+# calculation of distance travelled
+d = v * t
+
+# printing result
+print (f'velocity           = {v}')
+print (f'time               = {t}')
+print (f'distance travelled = {d}')
