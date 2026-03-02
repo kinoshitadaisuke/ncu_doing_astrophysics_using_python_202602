f6fe68313655db29f8039876f644d93cfe85f2c2
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:19:16 +0800

    adding "appy_s02_09_03.py" on  2/Mar/2026

--- s02/appy_s02_09_03.py
+++ s02/appy_s02_09_03.py
@@ -1,51 +1,30 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:06:28 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/28 21:19:19 (UT+08:00) daisuke>
 #
 
-# importing re module
-import re
+# importing urllib module
+import urllib.request
 
-# making a pattern for regular expression
-pattern_vega = re.compile ('Alp Lyr')
+# importing ssl module
+import ssl
 
-# catalogue file of BSC
-file_bsc = 'bsc.data'
+# allow insecure downloading
+ssl._create_default_https_context = ssl._create_unverified_context
 
-# opening file for reading
-with open (file_bsc, 'r') as fh:
-    # reading file line-by-line
-    for line in fh:
-        # pattern matching using regular expression
-        match_vega = re.search (pattern_vega, line)
-        # if matched, print the line
-        if (match_vega):
-            # HR number
-            HR    = line[0:4].strip ()
-            # name
-            name  = line[4:14].strip ()
-            # RAh
-            RAh   = line[75:77].strip ()
-            # RAm
-            RAm   = line[77:79].strip ()
-            # RAs
-            RAs   = line[79:83].strip ()
-            # Decpm
-            Decpm = line[83].strip ()
-            # Decd
-            Decd  = line[84:86].strip ()
-            # Decm
-            Decm  = line[86:88].strip ()
-            # Decs
-            Decs  = line[88:90].strip ()
-            # Vmag
-            Vmag  = line[102:107].strip ()
-            # SpType
-            SpType = line[127:147].strip ()
-            # RA
-            RA = f'{RAh}:{RAm}:{RAs}'
-            # Dec
-            Dec = f'{Decpm}{Decd}:{Decm}:{Decs}'
-            # printing information about star
-            print (f'{HR:4}  {name:10}  {RA}  {Dec}  {Vmag}  {SpType}')
+# URL of data file
+url_planets = 'https://s3b.astro.ncu.edu.tw/appy_202602/data/planets_solsys.data'
+
+# output data file
+file_planets = 'planets_solsys.data'
+
+# opening URL
+with urllib.request.urlopen (url_planets) as fh_in:
+    # reading data
+    data_planets = fh_in.read ()
+
+# opening file for writing
+with open (file_planets, 'w') as fh_out:
+    # writing data into file
+    fh_out.write (data_planets.decode ('utf-8'))
