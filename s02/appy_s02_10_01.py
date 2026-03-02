199b3641048fa1f7ae6a912efe612b15bf5f82dc
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:19:23 +0800

    adding "appy_s02_10_01.py" on  2/Mar/2026

--- s02/appy_s02_10_01.py
+++ s02/appy_s02_10_01.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:07:13 (UT+08:00) daisuke>
+# Time-stamp: <2026/03/01 11:07:40 (UT+08:00) daisuke>
 #
 
-# importing math module
-import math
+# importing json module
+import json
 
-# two floats "a" and "b"
-a = 12.34
-b = -56.78
+# json file
+file_json = 'stars.json'
 
-# floor
-a_floor = math.floor (a)
-b_floor = math.floor (b)
+# opening file for reading
+with open (file_json, 'r') as fh:
+    # reading json file
+    dic_data = json.load (fh)
 
-# printing results
-print (f'Use of floor () function:')
-print (f'  a         = {a}')
-print (f'  floor (a) = {a_floor}')
-print (f'  {a_floor} is the largest integer less than or equal to {a}.')
-print (f'  b         = {b}')
-print (f'  floor (b) = {b_floor}')
-print (f'  {b_floor} is the largest integer less than or equal to {b}.')
-print (f'')
+# printing data
+for key1 in sorted (dic_data.keys ()):
+    print (f'{key1}')
+    for key2 in sorted (dic_data[key1].keys ()):
+        print (f'  {key2} : {dic_data[key1][key2]}')
