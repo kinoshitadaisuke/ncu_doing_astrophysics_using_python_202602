4689328781f65268df314ecba8be7747d54d09a3
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:20:14 +0800

    adding "appy_s02_15_09.py" on  2/Mar/2026

---
+++ s02/appy_s02_15_09.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:08:28 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+# perm
+p_5_3 = math.perm (5, 3)
+p_8_4 = math.perm (8, 4)
+
+fac_2 = math.factorial (2)
+fac_4 = math.factorial (4)
+fac_5 = math.factorial (5)
+fac_8 = math.factorial (8)
+
+# printing results
+print (f'Use of perm () function:')
+print (f'  P (5, 3) = (5!) / (2!)')
+print (f'           = {fac_5} / {fac_2}')
+print (f'           = {p_5_3}')
+print (f'  P (8, 4) = (8!) / (4!)')
+print (f'           = {fac_8} / {fac_4}')
+print (f'           = {p_8_4}')
+print (f'')
