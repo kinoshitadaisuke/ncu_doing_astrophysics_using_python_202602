ed7ceab9b7996ca34f3f147d9019ba32740cb2b5
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:21:08 +0800

    adding "appy_s02_22_03.py" on  2/Mar/2026

---
+++ s02/appy_s02_22_03.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:13:34 (UT+08:00) daisuke>
+#
+
+# importing sympy module
+import sympy
+
+# variable x
+x, a, b, c = sympy.symbols ('x a b c')
+
+# function f
+f = a * x**2 + b * x + c
+
+# solving f(x) = 0
+sol = sympy.solve (f, x)
+
+# printing result
+print (f'solution of {f}=0:')
+print (f'x = {sol}')
