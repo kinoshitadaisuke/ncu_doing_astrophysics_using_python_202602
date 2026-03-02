dca855829fcf17282c0d1727b8eb6fc76107557f
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:21:22 +0800

    adding "appy_s02_22_13.py" on  2/Mar/2026

---
+++ s02/appy_s02_22_13.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:14:53 (UT+08:00) daisuke>
+#
+
+# importing sympy module
+import sympy
+
+# variable x
+x = sympy.symbols ('x')
+
+# function f(x)
+f = sympy.sqrt (4 - x**2)
+
+# integration of f(x) from 0 to 2
+I = sympy.integrate (f, (x, 0, 2))
+
+# printing result
+print (f'I = {I}')
