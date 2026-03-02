e0f28ca184c1a3b8bea29283efe2c7013b4680b5
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:19:13 +0800

    adding "appy_s02_09_01.py" on  2/Mar/2026

--- s02/appy_s02_09_01.py
+++ s02/appy_s02_09_01.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:06:06 (UT+08:00) daisuke>
+# Time-stamp: <2026/02/28 21:18:08 (UT+08:00) daisuke>
 #
 
-# importing pathlib module
-import pathlib
+# importing csv module
+import csv
 
-# file of Yale Bright Star Catalogue
-file_bsc = 'catalog.gz'
+# CSV file name
+file_csv = 'stars.csv'
 
-# making pathlib object
-path_bsc = pathlib.Path (file_bsc)
-
-# existence check of file
-if (path_bsc.exists ()):
-    print (f'File "{file_bsc}" exists.')
-    print (f'Downloading of Yale Bright Star Catalogue was successfully done!')
-else:
-    print (f'File "{file_bsc}" DOES NOT exist.')
-    print (f'Download Yale Bright Star Catalogue!')
+# opening file for reading
+with open (file_csv, 'r') as fh:
+    data_elements = csv.reader (fh)
+    # printing data
+    for line in data_elements:
+        # spectral type
+        spectype = line[0]
+        # effective temperature
+        try:
+            t_eff = float (line[1])
+        except:
+            continue
+        # absolute magnitude
+        try:
+            absmag = float (line[2])
+        except:
+            continue
+        # luminosity
+        try:
+            luminosity = float (line[3])
+        except:
+            continue
+        # printing data
+        print (f'{spectype}')
+        print (f'  effective temperature [K]     : {t_eff:7.1f}')
+        print (f'  absolute magnitude            : {absmag:+5.1f}')
+        print (f'  luminosity [solar luminosity] : {luminosity:10.3f}')
