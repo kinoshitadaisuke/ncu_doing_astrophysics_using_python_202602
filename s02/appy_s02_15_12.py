5a7a7cfdfec29901ee22a205c7dd808a6014a791
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:20:19 +0800

    adding "appy_s02_15_12.py" on  2/Mar/2026

---
+++ s02/appy_s02_15_12.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:08:52 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+# sqrt () function
+b = math.sqrt (3)
+
+# printing result
+print (f'sqrt (3) = {b}')
