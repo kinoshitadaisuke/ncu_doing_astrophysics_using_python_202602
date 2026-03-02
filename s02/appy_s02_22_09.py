ba753a080429db01abdf06575eda0bf78b7e122d
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:21:16 +0800

    adding "appy_s02_22_09.py" on  2/Mar/2026

---
+++ s02/appy_s02_22_09.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:14:18 (UT+08:00) daisuke>
+#
+
+# importing sympy module
+import sympy
+
+# variable x
+x = sympy.symbols ('x')
+
+# function f
+f = 1/x
+
+# integration of f(x)
+I = sympy.integrate (f, x)
+
+# printing result
+print (f'f(x)  = {f}')
+print (f'integration of f(x) = {I}')
