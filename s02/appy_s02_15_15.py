095062fe04c1c3e99488ce39e1a96eec1b5aba41
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:20:23 +0800

    adding "appy_s02_15_15.py" on  2/Mar/2026

---
+++ s02/appy_s02_15_15.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:09:28 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+# conversion from radian into degree
+f_rad = math.pi / 2.0
+f_deg = math.degrees (f_rad)
+
+# printing result
+print (f'{f_rad} rad = {f_deg} deg')
