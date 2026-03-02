81af7456bf151371e6880e9659844f24a6c28639
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:21:11 +0800

    adding "appy_s02_22_05.py" on  2/Mar/2026

---
+++ s02/appy_s02_22_05.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:13:48 (UT+08:00) daisuke>
+#
+
+# importing sympy module
+import sympy
+
+# variable x
+x = sympy.symbols ('x')
+
+# function f
+f = sympy.sin (x)
+
+# differentiation of f(x)
+df_dx = sympy.diff (f, x)
+
+# printing result
+print (f'f(x)  = {f}')
+print (f'df/dx = {df_dx}')
