fda30b2f8323ca7dc6dc1b1f973f63efb4e506a3
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:19:44 +0800

    adding "appy_s02_12_01.py" on  2/Mar/2026

--- s02/appy_s02_12_01.py
+++ s02/appy_s02_12_01.py
@@ -1,50 +1,21 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:10:45 (UT+08:00) daisuke>
+# Time-stamp: <2026/03/02 22:12:29 (UT+08:00) daisuke>
 #
 
-# importing statistics module
-import statistics
+# importing tarfile module
+import tarfile
 
-# importing random module
-import random
+# tar file
+file_tar = 'files.tar'
 
-# number of data in dataset2
-ndata = 1000
+# opening file for reading
+with tarfile.open (file_tar, 'r:*') as fh:
+    # getting file names in tar file
+    list_files = fh.getnames ()
 
-# making an empty list for a synthetic dataset
-dataset2 = []
-
-# distribution parameters
-a = 100.0
-b = 200.0
-
-# generation of a synthetic dataset using random number generator
-for i in range (ndata):
-    # generating a random number
-    r = random.uniform (a, b)
-    # appending generated random number to list
-    dataset2.append (r)
-
-# printing generated dataset
-print (f'dataset2:')
-for i in range (ndata):
-    if ( (i > 4) and (i < ndata - 1) ):
-        continue
-    elif (i == ndata - 1):
-        print (f'  ...')
-    print (f'  {dataset2[i]}')
-
-# number of data in dataset2
-print (f'number of data in dataset2 = {len (dataset2)}')
-        
-# calculation of mean, median, and standard deviation
-mean   = statistics.fmean (dataset2)
-median = statistics.median (dataset2)
-stddev = statistics.stdev (dataset2)
-
-# printing mean, median, and standard deviation
-print (f'mean of dataset2           = {mean:7.3f}')
-print (f'median of dataset2         = {median:7.3f}')
-print (f'stddev of dataset2         = {stddev:7.3f}')
+# for each file name in list
+for filename in list_files:
+    # printing file name
+    print (filename)
