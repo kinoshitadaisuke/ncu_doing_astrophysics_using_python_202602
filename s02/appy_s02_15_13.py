2681aeb873d1300198de78f922ee96db71951fa0
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:20:20 +0800

    adding "appy_s02_15_13.py" on  2/Mar/2026

---
+++ s02/appy_s02_15_13.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:09:01 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+# log () function
+c = math.log (100)
+
+# printing result
+print (f'log (100)   = {c}')
+
+# log () function
+d = math.log10 (100)
+
+# printing result
+print (f'log10 (100) = {d}')
