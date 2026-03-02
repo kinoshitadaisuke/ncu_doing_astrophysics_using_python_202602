1454942ba583a5f314914c687357b3cd64ff9f44
Daisuke <mhdf23888@gmail.com>
2026-03-02 22:20:17 +0800

    adding "appy_s02_15_11.py" on  2/Mar/2026

---
+++ s02/appy_s02_15_11.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:08:45 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+# pow () function
+a = math.pow (3, 4)
+
+# printing result
+print (f'3^4 = 3 * 3 * 3 * 3 = {a}')
