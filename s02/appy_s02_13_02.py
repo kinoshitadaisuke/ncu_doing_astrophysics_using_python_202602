9f4cb4bdb5177748c52a79fce0c4296beac97ef7
Daisuke <mhdf23888@gmail.com>
2026-03-03 10:26:02 +0800

    adding "appy_s02_13_02.py" on  3/Mar/2026

--- s02/appy_s02_13_02.py
+++ s02/appy_s02_13_02.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:11:26 (UT+08:00) daisuke>
+# Time-stamp: <2026/03/03 10:10:51 (UT+08:00) daisuke>
 #
 
-# importing decimal module
-import decimal
+# importing zipfile module
+import zipfile
 
-# calculation of sqrt (2.0)
-a = decimal.Decimal ('2.0')
-b = a.sqrt ()
+# zip file
+file_zip = 'files2.zip'
 
-# result of calculation
-print (f'a = {a}')
-print (f'b = sqrt (a) = sqrt ({a}) = {b}')
-
-# calculation of log10 (12.3)
-c = decimal.Decimal ('12.3')
-d = c.log10 ()
-
-# result of calculation
-print (f'c = {c}')
-print (f'd = log10 (c) = log10 ({c}) = {d}')
+# opening file for reading
+with zipfile.ZipFile (file_zip, 'r') as fh:
+    # extracting files
+    fh.extractall ()
