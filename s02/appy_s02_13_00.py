fd669c00ec7fbad358750fc5ebb91dbc2f98432f
Daisuke <mhdf23888@gmail.com>
2026-03-03 10:25:59 +0800

    adding "appy_s02_13_00.py" on  3/Mar/2026

--- s02/appy_s02_13_00.py
+++ s02/appy_s02_13_00.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:11:08 (UT+08:00) daisuke>
+# Time-stamp: <2026/03/03 10:00:24 (UT+08:00) daisuke>
 #
 
-# two floating point numbers "a" and "b"
-a = 1.2
-b = 2.4
+# importing urllib module
+import urllib.request
 
-# calculation of c = a + b
-c = a + b
+# importing ssl module
+import ssl
 
-# printing result of calculation
-print (f'{a} + {b}             = {c}')
+# allow insecure downloading
+ssl._create_default_https_context = ssl._create_unverified_context
 
-# the other calculations
-d = 1.1
-e = 1.1
-f = 1.1
-g = d + e
-h = d + e + f
-i = d + e + f - 3.3
-print (f'{d} + {e}             = {g}')
-print (f'{d} + {e} + {f}       = {h}')
-print (f'{d} + {e} + {f} - 3.3 = {i}')
+# URL of data file
+url_zipfile = 'https://s3b.astro.ncu.edu.tw/appy_202602/data/files2.zip'
+
+# output data file
+file_zipfile = 'files2.zip'
+
+# opening URL
+with urllib.request.urlopen (url_zipfile) as fh_in:
+    # reading data
+    data_zipfile = fh_in.read ()
+
+# opening file for writing
+with open (file_zipfile, 'wb') as fh_out:
+    # writing data into file
+    fh_out.write (data_zipfile)
