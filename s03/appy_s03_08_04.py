1c27892c3b7cd263bcedebcefd18c4f4c83ffac7
Daisuke <mhdf23888@gmail.com>
2026-03-04 10:06:03 +0800

    adding "appy_s03_08_04.py" on  4/Mar/2026

---
+++ s03/appy_s03_08_04.py
@@ -0,0 +1,20 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/14 20:11:53 (UT+08:00) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# making Numpy array
+a = numpy.array ([5.0, 3.0, 7.0, 4.0, 9.0, 8.0, 1.0, 6.0, 2.0, 0.0])
+
+# printing "a"
+print (f'a:\n{a}')
+
+# sorting by descending order
+b = numpy.flip (numpy.sort (a, kind="mergesort"))
+
+# printing "b"
+print (f'b = numpy.flip (numpy.sort (a, kind="mergesort")):\n{b}')
