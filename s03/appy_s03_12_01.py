6d4392cbc36d7a439964afb29e16604054793573
Daisuke <mhdf23888@gmail.com>
2026-03-04 10:06:44 +0800

    adding "appy_s03_12_01.py" on  4/Mar/2026

---
+++ s03/appy_s03_12_01.py
@@ -0,0 +1,17 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/14 20:22:54 (UT+08:00) daisuke>
+#
+
+# input data file
+file_input = 'numpy_00.data'
+
+# opening file for reading
+with open (file_input, 'r') as fh_in:
+    # reading data in the file line-by-line
+    for line in fh_in:
+        # removing line feed at the end of the line
+        line = line.strip ()
+        # printing data
+        print (f'{line}')
