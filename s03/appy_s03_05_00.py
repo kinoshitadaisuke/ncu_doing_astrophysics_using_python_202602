bb8ccaccfa06199929834a77474966b1c42f30e5
Daisuke <mhdf23888@gmail.com>
2026-03-04 10:05:24 +0800

    adding "appy_s03_05_00.py" on  4/Mar/2026

---
+++ s03/appy_s03_05_00.py
@@ -0,0 +1,23 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/14 20:03:07 (UT+08:00) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# making Numpy arrays using numpy.linspace ()
+a = numpy.linspace (0.0, 9.0, 10)
+b = numpy.linspace (10.0, 19.0, 10)
+
+# printing a and b
+print (f'a = {a}')
+print (f'b = {b}')
+
+# calculation
+# no need of using "for"
+c = a + b
+
+# printing c
+print (f'c = a + b = {c}')
