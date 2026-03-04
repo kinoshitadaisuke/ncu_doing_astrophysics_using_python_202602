052d131714312d41ac70637fbc1dccc3915da15c
Daisuke <mhdf23888@gmail.com>
2026-03-04 10:04:27 +0800

    adding "appy_s03_00_01.py" on  4/Mar/2026

---
+++ s03/appy_s03_00_01.py
@@ -0,0 +1,17 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/14 19:50:04 (UT+08:00) daisuke>
+#
+
+# importing Numpy module
+import numpy
+
+# making a Numpy array (ndarray)
+array_a = numpy.array ([0.1, 2.3, 4.5, 6.7, 8.9])
+
+# type of "array_a"
+type_array_a = type (array_a)
+
+# printing type of "array_a"
+print (f'type of "array_a" = {type_array_a}')
