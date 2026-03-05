eaa0803e5238cf67d062b9a85223570a789a1b6d
Daisuke <mhdf23888@gmail.com>
2026-03-05 11:33:23 +0800

    adding "appy_s03_12_08.py" on  5/Mar/2026

--- s03/appy_s03_12_08.py
+++ s03/appy_s03_12_08.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/14 20:25:38 (UT+08:00) daisuke>
+# Time-stamp: <2026/03/04 16:31:01 (UT+08:00) daisuke>
 #
 
 # input data file
