5ead492b89d0316730eeef0594fff0f0362a1b4e
Daisuke <mhdf23888@gmail.com>
2026-03-07 21:49:09 +0800

    adding "appy_s03_02_16.py" on  7/Mar/2026

--- s03/appy_s03_02_16.py
+++ s03/appy_s03_02_16.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:20:14 (UT+08:00) daisuke>
+# Time-stamp: <2026/03/04 16:19:52 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
 # making a Numpy array (ndarray) with a specified data type
-# complex64 : 64-bit complex number
+# numpy.dtype ('c8') : 64-bit complex number
 array_i = numpy.array ([1.0 + 2.0j, 3.0j, 4.0, 5.0 - 6.0j, -7.0 + 8.0j, \
                         -9.0 - 10.0j, -11.0j, -12.0, 13.0 + 14.0j, 15.0j], \
-                       dtype='complex64')
+                       dtype=numpy.dtype ('c8'))
 
 # printing Numpy array
 print (f'array_i:\n{array_i}')
