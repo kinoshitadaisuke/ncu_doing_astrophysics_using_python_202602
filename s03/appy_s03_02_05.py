f575b5d1d16c2e907f8690f34f65441e3295f4ef
Daisuke <mhdf23888@gmail.com>
2026-03-07 21:48:54 +0800

    adding "appy_s03_02_05.py" on  7/Mar/2026

--- s03/appy_s03_02_05.py
+++ s03/appy_s03_02_05.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:18:55 (UT+08:00) daisuke>
+# Time-stamp: <2026/03/07 21:36:26 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
 # making a Numpy array (ndarray) of a specified data type
-# numpy.dtype ('u2') : 16-bit unsigned integer
-array_g = numpy.array ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], \
-                       dtype=numpy.dtype ('u2') )
+# numpy.dtype ('i1') : 8-bit integer
+array_f = numpy.array ([0, 1, 2, 3, 4, 5, -1, -2, -3, -4, -129], \
+                       dtype=numpy.dtype ('i1') )
 
 # printing Numpy array
-print (f'array_g:\n{array_g}')
+print (f'array_f:\n{array_f}')
 
 # printing information
 print (f'information:')
-print (f'  ndim     = {array_g.ndim}')
-print (f'  size     = {array_g.size}')
-print (f'  shape    = {array_g.shape}')
-print (f'  dtype    = {array_g.dtype}')
-print (f'  itemsize = {array_g.itemsize} byte')
+print (f'  ndim     = {array_f.ndim}')
+print (f'  size     = {array_f.size}')
+print (f'  shape    = {array_f.shape}')
+print (f'  dtype    = {array_f.dtype}')
+print (f'  itemsize = {array_f.itemsize} byte')
