ef4e3b7ddb27a2616c186728b7a7885ab53cd77e
Daisuke <mhdf23888@gmail.com>
2026-03-10 20:26:59 +0800

    adding "appy_s04_06_01.py" on 10/Mar/2026

--- s04/appy_s04_06_01.py
+++ s04/appy_s04_06_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/21 20:20:35 (UT+08:00) daisuke>
+# Time-stamp: <2026/03/10 20:19:46 (UT+08:00) daisuke>
 #
 
 # input data file
