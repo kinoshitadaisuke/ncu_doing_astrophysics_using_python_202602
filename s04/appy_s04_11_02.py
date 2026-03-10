dd0110439c5f2198069fc9a2934d100d999ec364
Daisuke <mhdf23888@gmail.com>
2026-03-10 20:27:37 +0800

    adding "appy_s04_11_02.py" on 10/Mar/2026

--- s04/appy_s04_11_02.py
+++ s04/appy_s04_11_02.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/22 07:46:35 (UT+08:00) daisuke>
+# Time-stamp: <2026/03/10 20:23:12 (UT+08:00) daisuke>
 #
 
 # importing argparse module
