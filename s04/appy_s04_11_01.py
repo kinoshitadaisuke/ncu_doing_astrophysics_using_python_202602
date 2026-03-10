a2afd694c1fb3891b037564a7b0292e61df96239
Daisuke <mhdf23888@gmail.com>
2026-03-10 20:27:35 +0800

    adding "appy_s04_11_01.py" on 10/Mar/2026

--- s04/appy_s04_11_01.py
+++ s04/appy_s04_11_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/09/22 07:44:45 (UT+08:00) daisuke>
+# Time-stamp: <2026/03/10 20:23:04 (UT+08:00) daisuke>
 #
 
 # importing subprocess module
