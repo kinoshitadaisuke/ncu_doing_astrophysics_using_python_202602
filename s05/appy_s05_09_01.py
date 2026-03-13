9c873917ee0c3bae5023d2e1f1dc57945d4ec1a0
Daisuke <mhdf23888@gmail.com>
2026-03-13 10:42:06 +0800

    adding "appy_s05_09_01.py" on 13/Mar/2026

---
+++ s05/appy_s05_09_01.py
@@ -0,0 +1,14 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/30 21:50:04 (UT+08:00) daisuke>
+#
+
+# importing scipy module
+import scipy.special
+
+# calculation of Gamma (6.5)
+result = scipy.special.gamma (6.5)
+
+# printing result of calculation
+print (f'Gamma (6.5) = {result}')
