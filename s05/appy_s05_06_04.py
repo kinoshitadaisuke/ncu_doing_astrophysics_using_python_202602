813eb0a885ba1a456596305310fdcaf68839956d
Daisuke <mhdf23888@gmail.com>
2026-03-13 10:41:32 +0800

    adding "appy_s05_06_04.py" on 13/Mar/2026

---
+++ s05/appy_s05_06_04.py
@@ -0,0 +1,31 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/30 21:21:12 (UT+08:00) daisuke>
+#
+
+# importing subprocess module
+import subprocess
+
+# ffmpeg command
+ffmpeg = 'ffmpeg'
+
+# frame rate
+framerate = 30
+
+# number of threads
+n_thread = 1
+
+# ffmpeg options
+options_ffmpeg = f'-f image2 -i p2/p2_%08d.png -start_number 0' \
+    + f' -framerate {framerate} -an -vcodec libx264 -pix_fmt yuv420p' \
+    + f' -threads {n_thread}'
+
+# output file name
+file_output = 'p2.mp4'
+
+# command to be executed
+command_ffmpeg = f'{ffmpeg} {options_ffmpeg} {file_output}'
+
+# execution of command
+subprocess.run (command_ffmpeg, shell=True)
