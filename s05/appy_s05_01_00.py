6cb09cc9decf67bf682f247c0c008fc3552e8d0f
Daisuke <mhdf23888@gmail.com>
2026-03-13 10:40:34 +0800

    adding "appy_s05_01_00.py" on 13/Mar/2026

---
+++ s05/appy_s05_01_00.py
@@ -0,0 +1,32 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/09/30 20:50:24 (UT+08:00) daisuke>
+#
+
+# importing argparse module
+import argparse
+
+# importing scipy module
+import scipy.stats
+
+# constructing a parser object
+descr  = 'generating random numbers of uniform distribution between 0 and 1'
+parser = argparse.ArgumentParser (description=descr)
+
+# adding arguments
+parser.add_argument ('-n', '--number', type=int, default=1, \
+                     help='number of random numbers (default: 1)')
+
+# parsing arguments
+args = parser.parse_args ()
+
+# input parameters
+n = args.number
+
+# generating a set of random numbers of uniform distribution between 0.0 and 1.0
+ru = scipy.stats.uniform.rvs (size=n)
+
+# printing generated random numbers
+print (f'generated random numbers:')
+print (f'{ru}')
