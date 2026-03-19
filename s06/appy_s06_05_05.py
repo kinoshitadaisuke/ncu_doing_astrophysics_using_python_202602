76d160c11fa5e80e00ba3376cae6a1cf6d4ec05c
Daisuke <mhdf23888@gmail.com>
2026-03-19 10:43:59 +0800

    adding "appy_s06_05_05.py" on 19/Mar/2026

--- s06/appy_s06_05_05.py
+++ s06/appy_s06_05_05.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/10/03 13:26:43 (UT+08:00) daisuke>
+# Time-stamp: <2026/03/19 10:41:12 (UT+08:00) daisuke>
 #
 
 # importing sqlite module
