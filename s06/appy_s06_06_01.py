e5221be4cc5d95f6166053d17e8f758fe4285e5b
Daisuke <mhdf23888@gmail.com>
2026-03-20 14:06:11 +0800

    adding "appy_s06_06_01.py" on 20/Mar/2026

---
+++ s06/appy_s06_06_01.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/03/20 13:32:56 (UT+08:00) daisuke>
+#
+
+# importing urllib module
+import urllib.request
+
+# importing ssl module
+import ssl
+
+# allow insecure downloading
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# URL of data file
+url_data = 'https://cdsarc.cds.unistra.fr/ftp/J/AJ/137/4186/ReadMe'
+
+# output file name
+file_output = 'landolt_2009.readme'
+
+# printing status
+print (f'Now, fetching {url_data}...')
+
+# opening URL
+with urllib.request.urlopen (url_data) as fh_read:
+    # reading data
+    data_byte = fh_read.read ()
+
+# printing status
+print (f'Finished fetching {url_data}!')
+
+# converting raw byte data into string
+data_str = data_byte.decode ('utf-8')
+
+# printing status
+print (f'Now, writing data into file "{file_output}"...')
+
+# opening file for writing
+with open (file_output, 'w') as fh_write:
+    # writing data
+    fh_write.write (data_str)
+
+# printing status
+print (f'Finished writing data into file "{file_output}"!')
