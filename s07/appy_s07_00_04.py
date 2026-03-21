f2b240a264efc8f783b15c1df3bc9b89577cb52a
Daisuke <mhdf23888@gmail.com>
2026-03-21 20:44:53 +0800

    adding "appy_s07_00_04.py" on 21/Mar/2026

---
+++ s07/appy_s07_00_04.py
@@ -0,0 +1,26 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/10/13 13:11:37 (UT+08:00) daisuke>
+#
+
+# importing astropy module
+import astropy.constants
+
+# gravitational constant
+G = astropy.constants.G
+
+# printing G
+print (G, "\n")
+
+# Boltzmann constant
+k = astropy.constants.k_B
+
+# printing k
+print (k, "\n")
+
+# Stafan-Boltzmann constant
+sigma = astropy.constants.sigma_sb
+
+# printing sigma
+print (sigma, "\n")
