638a9ffb51c0a290b9af01965389586b5bf34672
Daisuke <mhdf23888@gmail.com>
2026-03-21 20:46:47 +0800

    adding "appy_s07_04_02.py" on 21/Mar/2026

---
+++ s07/appy_s07_04_02.py
@@ -0,0 +1,37 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/10/13 13:39:30 (UT+08:00) daisuke>
+#
+
+# importing astropy module
+import astropy.coordinates
+import astropy.time
+import astropy.units
+
+# using "DE440" for solar system ephemeris
+astropy.coordinates.solar_system_ephemeris.set ('de440')
+
+# units
+u_m = astropy.units.m
+
+# date/time in UTC
+t_str = '2025-11-01 04:00:00'
+t_utc = astropy.time.Time (t_str, format='iso', scale='utc')
+
+# location of observer: NCU main campus
+longitude = '121d11m12s'
+latitude  = '+24d58m12s'
+height    = 151.6 * u_m
+observer  = astropy.coordinates.EarthLocation (longitude, latitude, height)
+
+# getting position of the Sun
+sun = astropy.coordinates.get_body ('sun', t_utc, location=observer)
+
+# printing position of the Sun
+print (f'position of the Sun as observed at NCU main campus at {t_utc}:')
+print (f'  RA:  {int (sun.ra.hms.h):02d}:{int (sun.ra.hms.m):02d}', \
+       f':{sun.ra.hms.s:06.3f}', sep='')
+print (f'  Dec: {int (sun.dec.dms.d):02d}', \
+       f':{abs (int (sun.dec.dms.m)):02d}:{abs (sun.dec.dms.s):06.3f}', \
+       sep='')
