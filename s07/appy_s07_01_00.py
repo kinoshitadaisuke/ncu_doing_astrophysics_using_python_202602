e8fc80495909a8a31de81c168c6601d7a46a4655
Daisuke <mhdf23888@gmail.com>
2026-03-21 20:45:05 +0800

    adding "appy_s07_01_00.py" on 21/Mar/2026

---
+++ s07/appy_s07_01_00.py
@@ -0,0 +1,17 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/10/13 13:14:34 (UT+08:00) daisuke>
+#
+
+# importing astropy module
+import astropy.units
+
+# units
+u_sec = astropy.units.s
+
+# a quantity object of 900.0 sec
+t = 900.0 * u_sec
+
+# printing t
+print (f't = {t}')
