07e6126a726eadc5085e8046be97d9d4ebb4b9c1
Daisuke <mhdf23888@gmail.com>
2026-03-21 20:45:03 +0800

    adding "appy_s07_00_08.py" on 21/Mar/2026

---
+++ s07/appy_s07_00_08.py
@@ -0,0 +1,18 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/10/13 13:14:00 (UT+08:00) daisuke>
+#
+
+# importing astropy module
+import astropy.constants
+
+# Solar luminosity
+L_S = astropy.constants.L_sun
+
+# printing Solar radius, Jupiter radius, and Earth radius
+print (L_S, "\n")
+
+# amount of 10,000 Solar luminosity
+print (f'    1 L_S = {L_S:g}')
+print (f'10000 L_S = {10000 * L_S:g}')
