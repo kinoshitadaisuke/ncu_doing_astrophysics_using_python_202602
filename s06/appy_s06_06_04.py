6a363b46cd68aa5dace76a5517b4906130278596
Daisuke <mhdf23888@gmail.com>
2026-03-21 16:48:31 +0800

    adding "appy_s06_06_04.py" on 21/Mar/2026

--- s06/appy_s06_06_04.py
+++ s06/appy_s06_06_04.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/21 16:27:36 (UT+08:00) daisuke>
+# Time-stamp: <2026/03/21 16:46:04 (UT+08:00) daisuke>
 #
 
 # importing duckdb module
@@ -13,7 +13,6 @@ file_db = 'landolt_2009.db'
 # connecting to a new database
 with duckdb.connect (file_db) as connection:
     # getting a list of tables
-    list_tables = connection.sql ('SHOW TABLES').fetchall ()
+    tables = connection.sql ('SHOW TABLES').df ()
     # printing a list of tables
-    for table in list_tables:
-        print (table)
+    print (tables)
