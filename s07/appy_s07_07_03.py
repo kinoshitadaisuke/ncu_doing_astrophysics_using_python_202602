774834b65ee98d692b64e2753b2e08403152902d
Daisuke <mhdf23888@gmail.com>
2026-03-22 13:05:09 +0800

    adding "appy_s07_07_03.py" on 22/Mar/2026

--- s07/appy_s07_07_03.py
+++ s07/appy_s07_07_03.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/10/13 14:20:52 (UT+08:00) daisuke>
+# Time-stamp: <2026/03/22 13:00:30 (UT+08:00) daisuke>
 #
 
 # importing numpy
