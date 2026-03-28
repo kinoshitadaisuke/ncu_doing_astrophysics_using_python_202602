a62a6f186f7dbc3d654fbad52cdd924aca16428e
Daisuke <mhdf23888@gmail.com>
2026-03-28 20:54:31 +0800

    adding "appy_s07_02_14.py" on 28/Mar/2026

--- s07/appy_s07_02_14.py
+++ s07/appy_s07_02_14.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:55:19 (UT+08:00) daisuke>
+# Time-stamp: <2026/03/27 21:14:29 (UT+08:00) daisuke>
 #
 
 # importing astropy module
@@ -13,9 +13,9 @@ file_vot = 'exoplanet.vot'
 # reading VOTable file and making an Astropy table
 table_exoplanet = astropy.table.Table.read (file_vot)
 
-# printing information of planet discovered by direct imaging in 2023
+# printing information of planet discovered by direct imaging in 2025
 mask = (table_exoplanet["detection_type"] == "Imaging") \
-    & (table_exoplanet["discovered"] == 2023)
+    & (table_exoplanet["discovered"] == 2025)
 print (table_exoplanet[mask]["name", "mass", "semi_major_axis", \
                              "orbital_period", "detection_type", \
                              "discovered"])
