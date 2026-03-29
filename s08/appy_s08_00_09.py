6a82b998d41dd73db4cb9dd79ebb897378c59264
Daisuke <mhdf23888@gmail.com>
2026-03-29 11:36:52 +0800

    adding "appy_s08_00_09.py" on 29/Mar/2026

---
+++ s08/appy_s08_00_09.py
@@ -0,0 +1,45 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/10/20 13:36:11 (UT+08:00) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# importing scipy module
+import scipy.constants
+import scipy.optimize
+
+# speed of light in vacuum
+c = scipy.constants.physical_constants['speed of light in vacuum']
+
+# Planck constant
+h = scipy.constants.physical_constants['Planck constant']
+
+# Boltzmann constant
+k = scipy.constants.physical_constants['Boltzmann constant']
+
+# temperature of blackbody
+T = 5800.0
+
+# function for solving the equation (x-5) * exp(x) + 5 = 0
+def find_peak (x):
+    y = ( (x - 5.0) * numpy.exp (x) + 5 )**2
+    return (y)
+
+# finding the minimum x corresponding to the peak of blackbody radiation
+x_min = scipy.optimize.minimize_scalar (find_peak, bracket=(2.0, 5.0), \
+                                        method='Brent')
+
+# printing the minimum x
+print (x_min)
+
+# finding a peak of T=5800 K blackbody radiation spectrum
+wavelength_peak = h[0] * c[0] / (x_min.x * k[0] * T)
+
+# printing peak wavelength of black body radiation
+print (f'peak wavelength of T={T} K blackbody:')
+print (f'  lambda = {wavelength_peak} [m]')
+print (f'         = {wavelength_peak * 10**6} [micron]')
+print (f'         = {wavelength_peak * 10**9} [nm]')
