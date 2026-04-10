33e8b20ed479fa78ad808fa8cb60e65b87f1fef7
Daisuke <mhdf23888@gmail.com>
2026-04-10 13:20:09 +0800

    adding "appy_s09_00_12.py" on 10/Apr/2026

--- s09/appy_s09_00_12.py
+++ s09/appy_s09_00_12.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2025/10/23 01:15:05 (UT+08:00) daisuke>
+# Time-stamp: <2026/04/10 13:17:26 (UT+08:00) daisuke>
 #
 
 # importing numpy module
@@ -19,7 +19,7 @@ import astroquery.jplhorizons
 file_output = 'asteroids_001000.data'
 
 # date/time
-date = astropy.time.Time ('2026-01-01 00:00:00')
+date = astropy.time.Time ('2026-07-01 00:00:00')
 
 # number of asteroids to get position
 n_asteroids = 1000
