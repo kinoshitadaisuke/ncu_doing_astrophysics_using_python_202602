8f9a500830161b947024393d0811315db81095f5
Daisuke <mhdf23888@gmail.com>
2026-04-10 12:40:38 +0800

    adding "appy_s09_00_19.py" on 10/Apr/2026

---
+++ s09/appy_s09_00_19.py
@@ -0,0 +1,42 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/10/28 21:20:37 (UT+08:00) daisuke>
+#
+
+# importing urllib module
+import urllib.request
+
+# importing ssl module
+import ssl
+
+# allow insecure downloading
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# URL of data file
+url_data = 'https://s3b.astro.ncu.edu.tw/appy_202509/data/asteroids_100000.data'
+
+# output file name
+file_output = 'asteroids_100000.data'
+
+# printing status
+print (f'Now, fetching {url_data}...')
+
+# opening URL
+with urllib.request.urlopen (url_data) as fh_read:
+    # reading data
+    data_byte = fh_read.read ()
+
+# printing status
+print (f'Finished fetching {url_data}!')
+
+# printing status
+print (f'Now, writing data into file "{file_output}"...')
+
+# opening file for writing
+with open (file_output, 'wb') as fh_write:
+    # writing data
+    fh_write.write (data_byte)
+
+# printing status
+print (f'Finished writing data into file "{file_output}"!')
