ee423570c1a3fdc1ef2cdbefd2a36ec14c3a2b13
Daisuke <mhdf23888@gmail.com>
2026-04-13 09:29:12 +0800

    adding "appy_s09_01_01.py" on 13/Apr/2026

--- s09/appy_s09_01_01.py
+++ s09/appy_s09_01_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/10/28 22:01:09 (UT+08:00) daisuke>
+# Time-stamp: <2026/04/13 09:25:27 (UT+08:00) daisuke>
 #
 
 # importing urllib module
