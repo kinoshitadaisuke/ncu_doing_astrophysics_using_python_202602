dacd33725bff7b405105655a88c898beb92ca671
Daisuke <mhdf23888@gmail.com>
2026-04-17 11:44:11 +0800

    adding "appy_s10_02_02.py" on 17/Apr/2026

---
+++ s10/appy_s10_02_02.py
@@ -0,0 +1,20 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/11/03 14:12:55 (UT+08:00) daisuke>
+#
+
+# importing json module
+import json
+
+# JSON file name
+file_json = 'exoplanets/data/exoplanet.json'
+
+# opening file
+with open (file_json, 'r') as fh:
+    # reading JSON file
+    data = json.load (fh)
+
+# printing keys of the data
+for key in data[0].keys ():
+    print (f"{key}")
