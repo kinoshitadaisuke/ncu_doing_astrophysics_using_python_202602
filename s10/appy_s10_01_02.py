541426da910204169bad82a8c593cfa8d209abaa
Daisuke <mhdf23888@gmail.com>
2026-04-17 11:43:51 +0800

    adding "appy_s10_01_02.py" on 17/Apr/2026

---
+++ s10/appy_s10_01_02.py
@@ -0,0 +1,17 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/11/03 14:12:20 (UT+08:00) daisuke>
+#
+
+# importing astropy module
+import astropy.io.ascii
+
+# CSV file name
+file_csv = 'honey-badger/examples/planets/planets.csv'
+
+# reading a CSV file and storing data in an astropy table
+table = astropy.io.ascii.read (file_csv, format='csv')
+
+# printing column names of astropy table
+print (table.info ())
