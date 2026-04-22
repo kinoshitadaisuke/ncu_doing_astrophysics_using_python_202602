5409907b640cb91796b35ef6e38253648a892d4c
Daisuke <mhdf23888@gmail.com>
2026-04-22 08:54:36 +0800

    adding "appy_s11_04_00.py" on 22/Apr/2026

---
+++ s11/appy_s11_04_00.py
@@ -0,0 +1,23 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/11/07 10:05:09 (UT+08:00) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# importing astropy module
+import astropy.io.votable
+
+# VOTable file name
+file_votable = "m3.vot.gz"
+
+# output file name
+file_output = 'm3.list'
+
+# reading VOTable
+data_vot = astropy.io.votable.parse_single_table (file_votable)
+
+# printing data type
+print (f'data type of "data_vot" = {type (data_vot)}')
