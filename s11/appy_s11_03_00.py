c180b8434c17cf8995ee45cb3983bc070ec8dff0
Daisuke <mhdf23888@gmail.com>
2026-04-23 08:48:38 +0800

    adding "appy_s11_03_00.py" on 23/Apr/2026

--- s11/appy_s11_03_00.py
+++ s11/appy_s11_03_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/11/07 10:04:58 (UT+08:00) daisuke>
+# Time-stamp: <2026/04/23 08:43:18 (UT+08:00) daisuke>
 #
 
 # importing astropy module
