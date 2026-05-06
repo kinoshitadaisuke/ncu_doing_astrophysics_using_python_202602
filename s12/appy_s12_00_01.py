dc39ca9efa1fdff7aa4ccc7cfef2aef49abe86b2
Daisuke <mhdf23888@gmail.com>
2026-05-06 19:11:34 +0800

    adding "appy_s12_00_01.py" on  6/May/2026

---
+++ s12/appy_s12_00_01.py
@@ -0,0 +1,65 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/11/14 15:33:37 (UT+08:00) daisuke>
+#
+
+# importing matplotlib module
+import matplotlib.figure
+import matplotlib.backends.backend_agg
+
+# importing numpy module
+import numpy
+
+# data file name
+file_data = 'appy_s12_00_00.data'
+
+# output file name
+file_output = 'appy_s12_00_01.png'
+
+# MJD offset
+mjd_offset = 61000.0
+
+# empty numpy arrays for storing data
+data_mjd = numpy.array ([])
+data_mag = numpy.array ([])
+
+# opening file
+with open (file_data, 'r') as fh:
+    # reading file line-by-line
+    for line in fh:
+        # skipping line if the line starts with '#'
+        if (line[0] == '#'):
+            continue
+        # removing line feed at the end of line
+        line = line.strip ()
+        # splitting data
+        (datetime, mjd_str, mag_str) = line.split ()
+        # conversion from string into float
+        mjd = float (mjd_str)
+        mag = float (mag_str)
+        # appending the data at the end of numpy arrays
+        data_mjd = numpy.append (data_mjd, mjd)
+        data_mag = numpy.append (data_mag, mag)
+
+# making objects "fig" and "ax"
+fig    = matplotlib.figure.Figure ()
+canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
+ax     = fig.add_subplot (111)
+
+# labels
+ax.set_xlabel (f'MJD - {mjd_offset} [day]')
+ax.set_ylabel ('Apparent Magnitude [mag]')
+
+# range
+ax.set_ylim (19.3, 20.7)
+ax.invert_yaxis ()
+
+# plotting data
+ax.plot (data_mjd - mjd_offset, data_mag, \
+         linestyle='None', marker='o', markersize=3, color='blue', \
+         label='synthetic time-series data')
+ax.legend ()
+
+# saving the plot into a file
+fig.savefig (file_output, dpi=150)
