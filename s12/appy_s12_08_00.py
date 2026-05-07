c05c657c3e1ad8ee90b7cd3a12b84e6f3e154037
Daisuke <mhdf23888@gmail.com>
2026-05-07 17:32:46 +0800

    adding "appy_s12_08_00.py" on  7/May/2026

--- s12/appy_s12_08_00.py
+++ s12/appy_s12_08_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/11/17 14:51:14 (UT+08:00) daisuke>
+# Time-stamp: <2026/05/07 17:22:34 (UT+08:00) daisuke>
 #
 
 # importing urllib module
