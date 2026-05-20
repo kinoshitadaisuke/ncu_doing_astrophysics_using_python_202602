9af592817d9cfc0283b25f4d1d313ef0bbf6325e
Daisuke <mhdf23888@gmail.com>
2026-05-20 08:58:08 +0800

    adding "appy_s14_08_03.py" on 20/May/2026

---
+++ s14/appy_s14_08_03.py
@@ -0,0 +1,25 @@
+#!/usr/pkg/bin/python3
+
+#
+# Time-stamp: <2025/12/08 22:58:29 (UT+08:00) daisuke>
+#
+
+# importing subprocess module
+import subprocess
+
+# ffmpeg command
+ffmpeg = 'ffmpeg'
+
+# ffmpeg options
+options_ffmpeg = f'-f image2 -start_number 0 -framerate 30' \
+    + f' -i iss3d_cropped/iss3d_%06dc.png' \
+    + f' -an -vcodec libx264 -pix_fmt yuv420p -threads 4'
+
+# output file name
+file_output = 'iss3d.mp4'
+
+# command to be executed
+command_ffmpeg = f'{ffmpeg} {options_ffmpeg} {file_output}'
+
+# execution of command
+subprocess.run (command_ffmpeg, shell=True)
