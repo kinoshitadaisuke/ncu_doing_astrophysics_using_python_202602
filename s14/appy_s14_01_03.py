4c55fb5f0642bb2ddfc9b8d242eaffd3e07a4c1d
Daisuke <mhdf23888@gmail.com>
2026-05-20 11:20:09 +0800

    adding "appy_s14_01_03.py" on 20/May/2026

--- s14/appy_s14_01_03.py
+++ s14/appy_s14_01_03.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/12/09 19:52:51 (UT+08:00) daisuke>
+# Time-stamp: <2026/05/20 11:09:04 (UT+08:00) daisuke>
 #
 
 # importing astropy module
