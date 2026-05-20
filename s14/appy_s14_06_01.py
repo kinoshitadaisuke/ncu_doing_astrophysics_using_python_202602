2c19783da0d469719a98eeaf367f8b439469e656
Daisuke <mhdf23888@gmail.com>
2026-05-20 11:25:03 +0800

    adding "appy_s14_06_01.py" on 20/May/2026

--- s14/appy_s14_06_01.py
+++ s14/appy_s14_06_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/12/09 21:27:20 (UT+08:00) daisuke>
+# Time-stamp: <2026/05/20 11:12:45 (UT+08:00) daisuke>
 #
 
 # importing gzip module
