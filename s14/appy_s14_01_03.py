027c4623b871238e58f7c6ad9617cf852897c8b6
Daisuke <mhdf23888@gmail.com>
2026-05-20 08:51:23 +0800

    adding "appy_s14_01_03.py" on 20/May/2026

---
+++ s14/appy_s14_01_03.py
@@ -0,0 +1,51 @@
+#!/usr/pkg/bin/python3
+
+#
+# Time-stamp: <2025/12/09 19:52:51 (UT+08:00) daisuke>
+#
+
+# importing astropy module
+import astropy.time
+
+# importing astroquery module
+import astroquery.jplhorizons
+
+# target name
+target = 'Sun'
+
+# observatory (D35 ==> Lulin Observatory)
+#
+# To get a list of available observatory codes, visit following web page.
+#   https://www.minorplanetcenter.net/iau/lists/ObsCodes.html
+#
+obs_site = 'D35'
+
+# date/time
+t  = astropy.time.Time ('2026-01-01T04:00:00', scale='utc', format='isot')
+jd = t.jd
+
+# sending a query to NASA/JPL Horizons system
+obj = astroquery.jplhorizons.Horizons (id_type=None, id=target, \
+                                       location=obs_site, epochs=jd)
+
+# getting ephemerides
+ephemerides = obj.ephemerides ()
+
+# printing ephemerides
+print (f'Target body    = {ephemerides["targetname"][0]}')
+print (f'Observing site = {obs_site}')
+print (f'Ephemerides:')
+for data in ephemerides:
+    # printing date/time
+    print (f'  {data["datetime_str"]}')
+    print (f'    RA         = {data["RA"]} [deg]')
+    print (f'    Dec        = {data["DEC"]} [deg]')
+    print (f'    Ecl Lon    = {data["ObsEclLon"]} [deg]')
+    print (f'    Ecl Lat    = {data["ObsEclLat"]} [deg]')
+    print (f'    Gal Lon    = {data["GlxLon"]} [deg]')
+    print (f'    Gal Lat    = {data["GlxLat"]} [deg]')
+    print (f'    Az         = {data["AZ"]} [deg]')
+    print (f'    El         = {data["EL"]} [deg]')
+    print (f'    V-band mag = {data["V"]} [mag]')
+    print (f'    R          = {data["r"]} [au]')
+    print (f'    Delta      = {data["delta"]} [au]')
