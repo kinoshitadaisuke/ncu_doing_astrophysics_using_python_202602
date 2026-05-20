427c851199223a237c5977d8156ed4a245ed3386
Daisuke <mhdf23888@gmail.com>
2026-05-20 08:53:58 +0800

    adding "appy_s14_03_00.py" on 20/May/2026

---
+++ s14/appy_s14_03_00.py
@@ -0,0 +1,26 @@
+#!/usr/pkg/bin/python3
+
+#
+# Time-stamp: <2025/12/08 22:55:50 (UT+08:00) daisuke>
+#
+
+# importing rebound module
+import rebound
+
+# name of simulation file
+file_sim = 'star_planet.bin'
+
+# constructing a simulation object
+sim = rebound.Simulation ()
+
+# adding one solar mass star
+sim.add (m=1.0)
+
+# adding a planet of m=0.001, a=1, and e=0.3
+sim.add (m=10**-3, a=1.0, e=0.3)
+
+# printing simulation object
+print (sim)
+
+# saving simulation into a file
+sim.save_to_file (file_sim)
