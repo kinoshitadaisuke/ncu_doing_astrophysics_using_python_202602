871754f770625fa1b1688b3499179ab5365105cb
Daisuke <mhdf23888@gmail.com>
2026-05-20 11:26:02 +0800

    adding "appy_s14_07_03.py" on 20/May/2026

--- s14/appy_s14_07_03.py
+++ s14/appy_s14_07_03.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/12/08 22:57:55 (UT+08:00) daisuke>
+# Time-stamp: <2026/05/20 11:13:42 (UT+08:00) daisuke>
 #
 
 # importing datetime module
