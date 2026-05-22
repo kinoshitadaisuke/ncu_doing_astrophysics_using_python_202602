8056ea91b3c607e567f7fe558ab7e0c73bb40e27
Daisuke <mhdf23888@gmail.com>
2026-05-22 09:11:10 +0800

    adding "appy_s14_04_03.py" on 22/May/2026

--- s14/appy_s14_04_03.py
+++ s14/appy_s14_04_03.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/05/20 11:11:59 (UT+08:00) daisuke>
+# Time-stamp: <2026/05/21 22:04:03 (UT+08:00) daisuke>
 #
 
 # importing subprocess module
@@ -13,7 +13,7 @@ ffmpeg = 'ffmpeg'
 # ffmpeg options
 options_ffmpeg = f'-f image2 -start_number 0 -framerate 30' \
     + f' -i binary/binary_%08d.png' \
-    + f' -an -vcodec libx264 -pix_fmt yuv420p -threads 4'
+    + f' -an -vcodec libx264 -pix_fmt yuv420p -threads 16'
 
 # output file name
 file_output = 'binary.mp4'
