e0e698500f1e34f37c1d65ca786460ab703578f3
Daisuke <mhdf23888@gmail.com>
2026-05-22 09:11:38 +0800

    adding "appy_s14_06_05.py" on 22/May/2026

--- s14/appy_s14_06_05.py
+++ s14/appy_s14_06_05.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/05/20 11:13:12 (UT+08:00) daisuke>
+# Time-stamp: <2026/05/21 23:08:44 (UT+08:00) daisuke>
 #
 
 # importing subprocess module
@@ -13,7 +13,7 @@ ffmpeg = 'ffmpeg'
 # ffmpeg options
 options_ffmpeg = f'-f image2 -start_number 0 -framerate 30' \
     + f' -i trojan/trojan_%04d.png' \
-    + f' -an -vcodec libx264 -pix_fmt yuv420p -threads 4'
+    + f' -an -vcodec libx264 -pix_fmt yuv420p -threads 16'
 
 # output file name
 file_output = 'trojan.mp4'
