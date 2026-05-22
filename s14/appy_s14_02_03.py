9118785d6e1e1ea8b87020dd2a7aedc960c822f4
Daisuke <mhdf23888@gmail.com>
2026-05-22 09:10:15 +0800

    adding "appy_s14_02_03.py" on 22/May/2026

--- s14/appy_s14_02_03.py
+++ s14/appy_s14_02_03.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/05/20 11:10:27 (UT+08:00) daisuke>
+# Time-stamp: <2026/05/21 16:55:57 (UT+08:00) daisuke>
 #
 
 # importing astropy module
@@ -11,7 +11,7 @@ import astropy.time
 import astroquery.jplhorizons
 
 # date/time
-t_str = '2026-01-01T12:00:00'
+t_str = '2026-07-01T12:00:00'
 t     = astropy.time.Time (t_str, scale='utc', format='isot')
 
 # target list
