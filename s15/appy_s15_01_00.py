92230186ddf9761ab53a7c828d3bf2ce1e893256
Daisuke <mhdf23888@gmail.com>
2026-05-27 10:03:47 +0800

    adding "appy_s15_01_00.py" on 27/May/2026

---
+++ s15/appy_s15_01_00.py
@@ -0,0 +1,37 @@
+#!/usr/pkg/bin/python3
+
+#
+# Time-stamp: <2025/12/15 14:41:33 (UT+08:00) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# output file name
+file_output = 'appy_s15_01_00.data'
+
+# making a random number generator
+rng = numpy.random.default_rng ()
+
+# generating random numbers
+mean_0 = numpy.array ([20.0, 10.0])
+mean_1 = numpy.array ([15.0, 15.0])
+covar  = numpy.array ([ [6.0, 4.0], [4.0, 4.0] ])
+n_0    = 200
+n_1    = 200
+data_0 = rng.multivariate_normal (mean=mean_0, cov=covar, size=n_0)
+data_1 = rng.multivariate_normal (mean=mean_1, cov=covar, size=n_1)
+
+# opening file for writing
+with open (file_output, 'w') as fh:
+    # header of output file
+    header = f'# value of feature X, value of feature Y, classification\n'
+    # writing header to output file
+    fh.write (header)
+    # writing data to output file
+    for i in range (data_0.shape[0]):
+        record = f'{data_0[i,0]:8.4f} {data_0[i,1]:8.4f} A\n'
+        fh.write (record)
+    for i in range (data_1.shape[0]):
+        record = f'{data_1[i,0]:8.4f} {data_1[i,1]:8.4f} B\n'
+        fh.write (record)
