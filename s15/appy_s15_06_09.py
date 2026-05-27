46eea65a83e531d1cb808bca5ccaf8942e6ae22e
Daisuke <mhdf23888@gmail.com>
2026-05-27 10:07:07 +0800

    adding "appy_s15_06_09.py" on 27/May/2026

---
+++ s15/appy_s15_06_09.py
@@ -0,0 +1,27 @@
+#!/usr/pkg/bin/python3
+
+#
+# Time-stamp: <2025/12/15 14:44:49 (UT+08:00) daisuke>
+#
+
+# data file
+file_albedo = 'compil.ast.albedos/data/albedos.tab'
+
+# printing header
+print (f'# asteroid number, albedo, albedo error')
+
+# opening data file
+with open (file_albedo, 'r') as fh:
+    # reading data file line-by-line
+    for line in fh:
+        # extracting data
+        number          = int (line[0:6])
+        iras_albedo     = float (line[45:51])
+        iras_albedo_err = float (line[52:57])
+        quality_code    = int (line[58:59])
+        # rejecting data if no measurement done by IRAS
+        if (quality_code == 0):
+            continue
+        # printing data
+        if (number > 0):
+            print (f'{number:6d} {iras_albedo:5.3f} {iras_albedo_err:5.3f}')
