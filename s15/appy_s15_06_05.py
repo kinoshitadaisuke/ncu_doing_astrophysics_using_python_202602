29426c3a763f18ccb03ee6a480af50faca6925bd
Daisuke <mhdf23888@gmail.com>
2026-05-28 12:22:31 +0800

    adding "appy_s15_06_05.py" on 28/May/2026

--- s15/appy_s15_06_05.py
+++ s15/appy_s15_06_05.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/12/15 14:44:29 (UT+08:00) daisuke>
+# Time-stamp: <2026/05/28 12:16:50 (UT+08:00) daisuke>
 #
 
 # importing urllib module
