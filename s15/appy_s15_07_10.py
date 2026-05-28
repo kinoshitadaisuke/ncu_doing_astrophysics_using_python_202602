f320959b5b78027532e716e1f6e98853255c9950
Daisuke <mhdf23888@gmail.com>
2026-05-28 12:23:19 +0800

    adding "appy_s15_07_10.py" on 28/May/2026

--- s15/appy_s15_07_10.py
+++ s15/appy_s15_07_10.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3
+#!/usr/bin/env python3
 
 #
-# Time-stamp: <2025/12/16 11:18:44 (UT+08:00) daisuke>
+# Time-stamp: <2026/05/28 12:18:44 (UT+08:00) daisuke>
 #
 
 # importing scikit-learn module
